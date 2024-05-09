af9509e6ed2de71712289e40bbca49affe5577a6
lenayork <lenayork@tut.by>
2024-05-09 17:09:59 +0200

    feat: add styles to PreviousStepButton

--- packages/app/components/ui/PreviousStepButton.tsx
+++ packages/app/components/ui/PreviousStepButton.tsx
@@ -1,20 +1,27 @@
-import { TouchableOpacity, StyleSheet, Image } from 'react-native';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  ViewStyle,
+  StyleProp,
+  TextStyle,
+} from 'react-native';
 import { useState } from 'react';
 import BackArrowSVG from '../../lib/assets/BackArrowSVG';
 
 type PreviousStepButtonProps = {
   handleInput: (action: string) => void,
   action: string,
-  styles: {
-    circle: {
-      width: number,
-      height: number,
-      borderRadius: number,
-      paddingTop: number,
-      paddingLeft: number,
-      cursor: string,
-    }
-  }
+  // styles: {
+  //   circle: {
+  //     width: number,
+  //     height: number,
+  //     borderRadius: number,
+  //     paddingTop: number,
+  //     paddingLeft: number,
+  //     cursor: string,
+  //   }
+  // }
+  styles?: StyleProp<ViewStyle> | StyleProp<TextStyle>
 };
 
 const PreviousStepButton = (props: PreviousStepButtonProps) => {
@@ -23,17 +30,19 @@ const PreviousStepButton = (props: PreviousStepButtonProps) => {
   const [isPressed, setIsPressed] = useState<boolean>(false);
 
   const onPressInHandler = () => {
+    console.log("in")
     setIsPressed(true);
   };
 
   const onPressOutHandler = () => {
+    console.log("out")
     setIsPressed(false);
     handleInput(action);
   };
 
   return (
     <TouchableOpacity
-      style={[styles.circle, { backgroundColor: isPressed ? '#FB8F67' : '#FEDED2' }]}
+      style={[defaultStyle.circle, styles, { backgroundColor: isPressed ? '#FB8F67' : '#FEDED2' }]}
       onPressIn={onPressInHandler}
       onPressOut={onPressOutHandler}
       activeOpacity={1}
@@ -42,5 +51,16 @@ const PreviousStepButton = (props: PreviousStepButtonProps) => {
     </TouchableOpacity>
   );
 }
+
+const defaultStyle = StyleSheet.create({
+  circle: {
+    width: 45,
+    height: 45,
+    borderRadius: 50,
+    paddingTop: 11,
+    paddingLeft: 10,
+    cursor: 'pointer',
+  },
+})
 
 export default PreviousStepButton;
